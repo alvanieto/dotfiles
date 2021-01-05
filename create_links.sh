ff5cfdc316dbadd0ddff5030a9c6b41d376da4dc
Álvaro Nieto <alvaro@doofinder.com>
2021-01-05 10:24:39 +0100

    Replaced compton with picom

--- create_links.sh
+++ create_links.sh
@@ -7,13 +7,15 @@ do
     ln -s ~/proyectos/dotfiles/${file} ~/${file}
 done
 
-FILES="compton.conf config i3status.conf"
+FILES="config i3status.conf"
 
 mkdir -p ~/.config/i3
 for file in ${FILES}
 do
     ln -s ~/proyectos/dotfiles/.config/i3/${file} ~/.config/i3/${file}
 done
+
+ln -s ~/proyectos/dotfiles/.config/picom.conf ~/.config/picom.conf
 
 mkdir -p ~/.config/nvim
 ln -s ~/proyectos/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
